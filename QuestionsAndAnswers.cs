691e9baa839c5022c04535175dfe18e9d9c96748
Seunghyo123 <lsh040506@gmail.com>
2024-06-25 23:31:43 +0900

    Create QuestionsAndAnswers.cs

---
+++ QuestionsAndAnswers.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[System.Serializable]
+
+public class QuestionsAndAnswers
+{
+    public string Question;
+    public string Subtitle;
+    public string[] Answers;
+    public int CorrectAnswer;
+    public Sprite QuestionImage;
+}
